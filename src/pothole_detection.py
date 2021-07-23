a6cdee9848d1f2935e9bfd3efe0f2ca3c6ae25ac
suneet1212 <suneets2000@gmail.com>
2021-07-23 16:28:32 +0530

    msg type created

--- src/pothole_detection.py
+++ src/pothole_detection.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 import rospy, cv2, cv_bridge
 import numpy as np
 from sensor_msgs.msg import  Image
 from cv_bridge import CvBridge
 import imutils
+from Gazebo_Virat.msg import Centres
 
 bridge = CvBridge()
 def callback(msg):
@@ -15,16 +16,18 @@ def callback(msg):
     mask_w = cv2.inRange(gray, 240, 255)
     cnts = cv2.findContours(mask_w, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
+    centres = Centres()
     for c in cnts:
-        if(cv2.contourArea(c) > 150):
+        if(cv2.contourArea(c) > 500):
             x,y,w,h = cv2.boundingRect(c)
-            cv2.rectangle(img,(int(x),int(y)),(int(x+w),int(y+h)),(0,0,255),2)
-            
-            org = (int(x),int(y))
-            cv2.putText(img,str(int(cv2.contourArea(c))),org,cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255))
-            cv2.imshow("img",img)
-            
-
+            cv2.rectangle(img,(int(x+w/2),int(y+h/2)),(int(x+5+w/2),int(y+5+h/2)),(0,0,255),-1)
+            centres.x.append(int(x+w/2))
+            centres.y.append(int(y+h/2))
+            # org = (int(x),int(y))
+            # cv2.putText(img,str(int(cv2.contourArea(c))),org,cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255))
+    cv2.imshow("img",img)
+    cv2.waitKey(1)
+    pub.publish(centres)
     # cnt = cnts[0]
     # cv2.imshow("Show",img)
 
@@ -45,4 +48,5 @@ def callback(msg):
 if __name__ == '__main__':
     rospy.init_node('object_detection')
     sub = rospy.Subscriber('/virat/camera1/image_raw', Image, callback)
+    pub = rospy.Publisher('/virat/camera_detected_points',Centres,queue_size=100)
     rospy.spin()
