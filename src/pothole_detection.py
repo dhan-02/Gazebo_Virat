01e0bb6e7141498e1d246be4147e25e0ce7c02d7
Dhanush <dhanush.anantharaman@gmail.com>
2021-07-23 20:39:42 +0530

    Class Implementation

--- src/pothole_detection.py
+++ src/pothole_detection.py
@@ -1,52 +1,50 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import rospy, cv2, cv_bridge
 import numpy as np
-from sensor_msgs.msg import  Image
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
 from cv_bridge import CvBridge
 import imutils
 from Gazebo_Virat.msg import Centres
+from image_geometry import PinholeCameraModel as pcm
 
-bridge = CvBridge()
-def callback(msg):
-    img = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-    # cv2.waitKey(3)
-    img = cv2.medianBlur(img,5)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    mask_w = cv2.inRange(gray, 240, 255)
-    cnts = cv2.findContours(mask_w, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    centres = Centres()
-    for c in cnts:
-        if(cv2.contourArea(c) > 500):
-            x,y,w,h = cv2.boundingRect(c)
-            cv2.rectangle(img,(int(x+w/2),int(y+h/2)),(int(x+5+w/2),int(y+5+h/2)),(0,0,255),-1)
-            centres.x.append(int(x+w/2))
-            centres.y.append(int(y+h/2))
-            # org = (int(x),int(y))
-            # cv2.putText(img,str(int(cv2.contourArea(c))),org,cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255))
-    cv2.imshow("img",img)
-    cv2.waitKey(1)
-    pub.publish(centres)
-    # cnt = cnts[0]
-    # cv2.imshow("Show",img)
-
-    # # cv2.imshow("gray", gray)
-    # # cv2.waitKey(1)
-    # circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
-    # # inverse ratio = 1
-    # # min dist between circles is set to 50
-    # # print(circles)
+class Pothole_Detection:
     
-    # if circles is not None:
-    #     circles = np.round(circles[0, :]).astype("int")
-	# for (x, y, r) in circles:
-	# 	cv2.rectangle(img, (x - 5, y - 5), (x + 5, y + 5), (0, 0, 255), -1)
-	# 	cv2.imshow("frame",img)
-	# 	cv2.waitKey(1)
-
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.virat_camera = pcm() 
+        rospy.init_node('object_detection')
+        rospy.Subscriber('/virat/camera1/image_raw', Image, self.callback_image)
+        rospy.Subscriber('/virat/camera1/camera_info', CameraInfo, self.callback_info)
+        self.pub = rospy.Publisher('/virat/camera_detected_points',Centres,queue_size=100)
+        
+    def callback_image(self,msg):
+        img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        # cv2.waitKey(3)
+        img = cv2.medianBlur(img,5)
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        mask_w = cv2.inRange(gray, 240, 255)
+        cnts = cv2.findContours(mask_w, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        centres = Centres()
+        for c in cnts:
+            if(cv2.contourArea(c) > 500):
+                x,y,w,h = cv2.boundingRect(c)
+                cv2.rectangle(img,(int(x+w/2),int(y+h/2)),(int(x+5+w/2),int(y+5+h/2)),(0,0,255),-1)
+                centres.x.append(int(x+w/2))
+                centres.y.append(int(y+h/2))
+                # org = (int(x),int(y))
+                # cv2.putText(img,str(int(cv2.contourArea(c))),org,cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255))
+        cv2.imshow("img",img)
+        cv2.waitKey(1)
+        self.pub.publish(centres)
+        
+    def callback_info(self,info_msg):
+        self.virat_camera.fromCameraInfo(info_msg)
+    
+   
 if __name__ == '__main__':
-    rospy.init_node('object_detection')
-    sub = rospy.Subscriber('/virat/camera1/image_raw', Image, callback)
-    pub = rospy.Publisher('/virat/camera_detected_points',Centres,queue_size=100)
+    
+    detector = Pothole_Detection()
     rospy.spin()
