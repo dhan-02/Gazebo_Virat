0a0ab51fa3d222eb1b7209a6370ad79993284c40
Dhanush <dhanush.anantharaman@gmail.com>
2021-07-23 14:32:07 +0530

    Mask

--- src/pothole_detection.py
+++ src/pothole_detection.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-#!/usr/bin/env python3
 import rospy, cv2, cv_bridge
 import numpy as np
 from sensor_msgs.msg import  Image
@@ -10,48 +9,13 @@ bridge = CvBridge()
 def callback(msg):
     img = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
     # cv2.waitKey(3)
-    #img = cv2.imread("/home/Ros_full/catkin_ws/src/Gazebo_Virat/src/a.png",1)
     img = cv2.medianBlur(img,5)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-     
-    # Threshold of blue in HSV space
-    lower = np.array([200, 200, 200])
-    upper = np.array([255, 255, 255])
-    cv2.imshow("img",img)
-    cv2.waitKey(1)
-    # preparing the mask to overlay
-    # mask = cv2.inRange(hsv, lower, upper)
-    # cv2.imshow("orig",mask)
-    # cv2.waitKey(1)
-    # The black region in the mask has the value of 0,
-    # so when multiplied with original image removes all non-blue regions
-    # gray = cv2.bitwise_and(img, img, mask = mask)
-
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-<<<<<<< HEAD
-    cv2.imshow("HI",gray)
-    cv2.waitKey(1)
     mask_w = cv2.inRange(gray, 240, 255)
     cv2.imshow("frame",mask_w)
     cv2.waitKey(1)
-    
-    # cv2.imshow("gray", gray)
-    # cv2.waitKey(1)
-    """circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,100,param1=100,param2=30,minRadius=10,maxRadius=500)
-    # inverse ratio = 1
-    # min dist between circles is set to 50
-    # print(circles)
-    print("HI")
-    if circles is not None:
-        circles = np.round(circles[0, :]).astype("int")
-        for (x, y, r) in circles:
-            cv2.rectangle(img, (x - 5, y - 5), (x + 5, y + 5), (0, 0, 255), -1)
-            cv2.imshow("frame",img)
-            cv2.waitKey(1)
-            print(x,y)"""
-            
-=======
-    edged = cv2.Canny(gray, 30, 200)
+    edged = cv2.Canny(mask_w, 30, 200)
     
     cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
@@ -82,7 +46,6 @@ def callback(msg):
 	# 	cv2.imshow("frame",img)
 	# 	cv2.waitKey(1)
 
->>>>>>> e76796787bb97607150db7288674ccaf274f4f9e
 if __name__ == '__main__':
     rospy.init_node('object_detection')
     sub = rospy.Subscriber('/virat/camera1/image_raw', Image, callback)
